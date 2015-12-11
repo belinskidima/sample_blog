d44abd1787a8f010fcecb5ac54a83a6a6a77101d
Belinschi Dima <belinski.d@gmail.com>
2015-12-11 18:45:10 +0200

    Add contacts controller and views new.erb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   get '/' =>'home#index'
 
+  get '/contacts' => 'contacts#new'
+
   resources :articles
+  resource :contacts, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
