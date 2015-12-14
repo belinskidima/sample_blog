7cc96ac5b7471055c80d5c5da672e986f4692e25
Belinschi Dima <belinski.d@gmail.com>
2015-12-14 18:02:42 +0200

    impliment articles#edit, articles#update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,34 @@
 class ArticlesController < ApplicationController
 
-  def index
-    @articles = Article.all
-  end
-
   def show
     @article = Article.find(params[:id])
   end
 
   def new
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to @article
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article =Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else 
+      render action: 'new'
+    end    
   end
 
   def create
@@ -17,7 +37,7 @@ class ArticlesController < ApplicationController
        @article.save
        redirect_to @article
     else
-      render action: 'new'
+      render action: 'edit'
     end
   end
 
